7233ca2ae995a63c88c56854a31a796a88e343af
Sripriya <patibandasripriya@gmail.com>
2024-08-22 17:05:02 +0530

    Updated Student Component

--- Project/src/app/teacher/teacher.component.ts
+++ Project/src/app/teacher/teacher.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+// src/app/components/teacher/teacher.component.ts
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
 
 @Component({
   selector: 'app-teacher',
   templateUrl: './teacher.component.html',
   styleUrls: ['./teacher.component.scss']
 })
-export class TeacherComponent {
+export class TeacherComponent implements OnInit {
+  teachers: any[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.loadTeachers();
+  }
+
+  loadTeachers() {
+    this.dataService.getTeachers().subscribe(data => this.teachers = data);
+  }
+
+  deleteTeacher(id: number) {
+    this.dataService.deleteRecord(id, 'teacher').subscribe(() => this.loadTeachers());
+  }
 }
+
+
